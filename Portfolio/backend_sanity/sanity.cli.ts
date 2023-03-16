17f9331b77202279c86197020f3fb121b1358f44
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-17 00:22:45 +0500

    init

---
+++ Portfolio/backend_sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8ccw2m6g',
+    dataset: 'production'
+  }
+})
---
+++ Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.scss'
+import { images } from '../../constants'
+const Navbar = () => {
+  return (
+<nav>
+    <div>
+        <img src={images.logo} alt='logo'/>
+    </div>
+    <ul>
+        {['home' ,'about','contact','work','skill'].map((item)=>(
+            <li key={`link-${item}`}>
+                <div />
+                <a href={`#${item}`}>{item}</a>
+            </li>
+        ))}
+    </ul>
+</nav>
+  )
+}
+
+export default Navbar
