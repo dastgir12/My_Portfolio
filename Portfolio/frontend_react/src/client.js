cc6e5277e60cdaa16c67e58201b05063fd812100
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-23 15:48:38 +0500

    init

--- Portfolio/frontend_react/src/client.js
+++ Portfolio/frontend_react/src/client.js
@@ -1,6 +1,5 @@
 import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
-
 export const client = createClient({
   projectId: '8ccw2m6g', // find this at manage.sanity.io or in your sanity.json
   dataset: 'production', // this is from those question during 'sanity init'
@@ -8,9 +7,7 @@ export const client = createClient({
   useCdn: true,
   token: 'skuuat9iejKQpRky6DVSAUA0Fj6OOYFKxbr5lWShN2b8CSl2WKQD9RFj63HWp8JHQDJRAOPNzfK5Vj2NvvG7Qn6GVvhNDV0H2UFWORUDtIE0kGvWkHtzMANrPOTAnVURRVeOz04Qd3qKBe4J7qdlMrOTxr2ps1fsIChidGBxNfGQDx19nvb3',
 });
-
 const builder = imageUrlBuilder(client);
-
 export const urlFor = (source) => {
   if (!source) {
     return '';
