5af5a8c1796f8042cbe769895905920495bac02f
dastgir12 <111153739+dastgir12@users.noreply.github.com>
2023-03-24 19:08:20 +0500

    init

--- Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
+++ Portfolio/frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,10 @@
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-
 import { images } from '../../constants';
 import './Navbar.scss';
-
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -21,10 +18,8 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
@@ -46,5 +41,4 @@ const Navbar = () => {
     </nav>
   );
 };
-
 export default Navbar;
